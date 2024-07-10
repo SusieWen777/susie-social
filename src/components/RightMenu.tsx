c86d86bae2fcf101bb41b6c7c981da170a29422a
SusieWen777 <wen_bufan11@163.com>
2024-07-10 12:49:03 +1000

    Done with User Info and User Media parts in the right menu

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,11 +1,19 @@
 import Ad from "./Ad";
 import Birthdays from "./Birthdays";
 import FriendRequests from "./FriendRequests";
+import UserInfoCard from "./UserInfoCard";
+import UserMediaCard from "./UserMediaCard";
 
 function RightMenu({ userId }: { userId?: string }) {
   //using userId to differentiate the homepage and profile page
   return (
     <div className="flex flex-col gap-6">
+      {userId ? (
+        <>
+          <UserInfoCard userId={userId} />
+          <UserMediaCard userId={userId} />
+        </>
+      ) : null}
       <FriendRequests />
       <Birthdays />
       <Ad size="md" />
---
+++ src/components/UserMediaCard.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function UserMediaCard({ userId }: { userId: string }) {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* Title */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">User Media</span>
+        <Link href="/" className="text-blue-500 text-sx">
+          See all
+        </Link>
+      </div>
+      {/* Images */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/14666143/pexels-photo-14666143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/7723697/pexels-photo-7723697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2265880/pexels-photo-2265880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/7150966/pexels-photo-7150966.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/21972291/pexels-photo-21972291/free-photo-of-close-up-of-an-australian-water-dragon-standing-on-the-ground.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/2869503/pexels-photo-2869503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/1058964/pexels-photo-1058964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/17208441/pexels-photo-17208441/free-photo-of-australian-endemic-kookaburra-bird.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserMediaCard;
